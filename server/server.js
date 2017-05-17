525a162a714fa69d782b4ebea2b103973ada3140
mikey neilson <mikeyneilson35@googlemail.com>
2017-05-17 22:48:58 +0100

    Fixed PORT typo in server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
 var app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
