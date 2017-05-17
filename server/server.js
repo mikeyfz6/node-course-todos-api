03a6696eec2449c57557d9ee99782f7f9fca1210
mikey neilson <mikeyneilson35@googlemail.com>
2017-05-17 18:56:45 +0100

    Add queries playground

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-
-
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
